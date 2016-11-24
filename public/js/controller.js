3e730701274a06086aa5f85ff54733d90e41d2a6
guminji <541814954@qq.com>
2016-11-24 17:57:23 +0800

    update

--- public/js/controller.js
+++ public/js/controller.js
@@ -2,11 +2,27 @@
  * Created by guminji on 2016/11/10 0010.
  */
 
-var app = angular.module('app',[]);
-app.controller('test',['$scope','native',function($scope,native,getCookies){
+var app = angular.module('app',['HDhttp']);
+app.controller('test',['$scope','native','httpMethod',function($scope,native,httpMethod){
     $scope.showMessage ='hello world';
     $scope.showPage =function(){
         native.showPage($scope.showMessage);
+    }
+    $scope.getHttp =function(){
+        console.log(httpMethod());
+        httpMethod().then(function(res){
+            console.log(res);
+        },
+        function(res){
+            console.log(res);
+        }
+        ).then(
+            function(){
+                console.log('promise chain!');
+            },function(){
+                console.log('cant load promise chain!')
+            }
+        );
     }
 }])
 //module.exports = app;
